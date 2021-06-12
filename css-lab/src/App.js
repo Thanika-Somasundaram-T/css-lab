4710b6ab7795001fa5d80e495264944e8561ae20
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-06-12 13:37:55 +0530

    Created Windows Logo.

---
+++ css-lab/src/App.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import './App.css';
+
+const App = () =>
+	<div className="App">
+		<div className=" box b1">1</div>
+		<div className="box  b2">2</div>
+		<div className="box b3">3</div>
+		<div className="box b4">4</div>
+	</div>
+;
+
+export default App;
---
+++ css-lab/src/core/seed.js
@@ -0,0 +1,8 @@
+// import config from './config';
+// import { rndString } from '@laufire/utils/random';
+
+const seed = {
+
+};
+
+export default seed;
