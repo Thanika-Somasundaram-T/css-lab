06cfa773d0474ca394fbb2ea71256843bdfdf5ff
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-06-15 19:04:23 +0530

    Added Border Radius and refactored skew.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className=" box b1">1</div>
-		<div className="box  b2">2</div>
-		<div className="box b3">3</div>
-		<div className="box b4">4</div>
+		<div className=" box b1"/>
+		<div className="box b2"/>
+		<div className="box b3"/>
+		<div className="box b4"/>
 	</div>
 ;
 
