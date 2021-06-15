a4a9cc3f501971b31780687a973afa55cf474ac9
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-06-15 20:32:01 +0530

    made circle as parent to logo.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="circle"/>
-		<div className=" box b1"/>
-		<div className="box b2"/>
-		<div className="box b3"/>
-		<div className="box b4"/>
+		<div className="circle">
+			<div className=" box b1"/>
+			<div className="box b2"/>
+			<div className="box b3"/>
+			<div className="box b4"/>
+		</div>
 	</div>
 ;
 
