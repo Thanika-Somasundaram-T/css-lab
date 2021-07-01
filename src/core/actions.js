32090f8b0f476abd9c999482c0a5b199cfbbcf62
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-07-01 19:43:26 +0530

    Refactored.

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,9 +1,9 @@
-const themeSelector = ({ data }) => ({
+const setTheme = ({ data }) => ({
 	theme: data,
 });
 
 const actions = {
-	themeSelector,
+	setTheme,
 };
 
 export default actions;
--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,7 @@
-/* eslint-disable max-len */
 const numberCount = 10;
-const colorCountMax = 200;
-const colorCountMin = 100;
 
 const config = {
 	randomNumber: Math.floor(Math.random() * numberCount) + 1,
-	randomTheme: Math.floor(Math.random() * (colorCountMax - colorCountMin)) + colorCountMin,
 	boxTheme: ['zebra', 'rainbow'],
 };
 
